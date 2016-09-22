f85c2e0a543acc5559573eb44ca89dcb8f1ec963
adrsamanta <alan.samanta@gmail.com>
2016-09-22 13:45:19 -0500

    working on creating user groups

--- server/methods.js
+++ server/methods.js
@@ -2,4 +2,16 @@
  * Created by alan on 9/13/2016.
  */
 
+//returns if the current user is admin
+var isAdmin = function () {
+    var adminUser = Meteor.users.findOne({username: "admin"});
+    return !!adminUser;
+};
+
+Meteor.methods({
+    startExperiment: function () {
+
+    }
+});
+
 
