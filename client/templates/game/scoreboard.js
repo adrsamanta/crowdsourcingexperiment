cc2fc39e4d5435c61620760fe64a533c14e77f4f
adrsamanta <alan.samanta@gmail.com>
2016-09-22 13:59:06 -0500

    standardized collections

--- client/templates/game/scoreboard.js
+++ client/templates/game/scoreboard.js
@@ -5,7 +5,7 @@
 Template.scoreboard.helpers({
     "scores": function () {
         let user = Meteor.user();
-        let userGroup = userGroups.find({users: {$in: [user.id]}});
+        let userGroup = userGroups.find({userids: {$in: [user.id]}});
         let neighbor_users = [];
         userGroup.forEach(function (e) {
             neighbor_users.concat(e.userids);
